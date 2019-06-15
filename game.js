180a9d9a79ff789185038de62e208c3de29dcb3d
Nate Sampo <nathan.sampo@students.olin.edu>
2019-06-15 17:43:28 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,369 @@
+navySeals = "What the fuck did you just fucking say about me, you little bitch? I\'ll have you know I graduated top of my class in the Navy Seals, and I\'ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and Im the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You\'re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and thats just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little clever comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldnt, you didn\'t, and now youre paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You\'re fucking dead, kiddo.";
+rickMorty = "To be fair, you have to have a very high IQ to understand Rick and Morty. The humor is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewer\'s head. There\'s also Rick\'s nihilistic outlook, which is deftly woven into his characterisation his personal philosophy draws heavily from Narodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realize that they\'re not just funny- they say something deep about LIFE. As a consequence people who dislike Rick and Morty truly ARE idiots- of course they wouldn\'t appreciate, for instance, the humour in Rick\'s existencial catchphrase \"Wubba Lubba Dub Dub,\" which itself is a cryptic reference to Turgenev\'s Russian epic Fathers and Sons I\'m smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Dan Harmon\'s genius unfolds itself on their television screens. What fools... how I pity them. And yes by the way, I DO have a Rick and Morty tattoo. And no, you cannot see it. It\'s for the ladies\' eyes only- And even they have to demonstrate that they\'re within 5 IQ points of my own (preferably lower) beforehand.";
+chickenNuggets = "Holy shit. My mom came into my room to bring me a plate of chicken nuggets and I literally screamed at her and hit the plate of chicken nuggets out of her hand. She started yelling and swearing at me and I slammed the door on her. I\'m so distressed right now I don\'t know what to do. I didn\'t mean to do that to my mom but I\'m literally in shock from the results tonight. I feel like I\'m going to explode. Why the fucking fuck is he losing? This can\'t be happening. I\'m having a fucking breakdown. I don\'t want to believe the world is so corrupt. I want a future to believe in. I want Bernie to be president and fix this broken country. I cannot fucking deal with this right now. It wasn\'t supposed to be like this, I thought he was polling well in New York???? This is so fucked.";
+rawr = "Rawr x3 *nuzzles* how are you *pounces on you* you\'re so warm o3o *notices you have a bulge* o: someone\'s happy ;) *nuzzles your necky wecky~* murr~ hehehe *rubbies your bulgy wolgy* you\'re so big :oooo *rubbies more on your bulgy wolgy* it doesn\'t stop growing *kisses you and lickies your necky* daddy likies *nuzzles wuzzles* I hope daddy really likes *wiggles butt and squirms* I want to see your big daddy meat~ *wiggles butt* I have a little itch o3o *wags tail* can you please get my itch~ *puts paws on your chest* nyea~ its a seven inch itch *rubs your chest* can you help me pwease *squirms* pwetty pwease *sad face* I need to be punished *runs paws down your chest and bites lip* like I need to be punished really good~ *paws on your bulge as I lick my lips* I\'m getting thirsty. I can go for some milk *unbuttons your pants as my eyes glow* you smell so musky *licks shaft* mmmm~ so musky *drools all over your cock* your daddy meat I like *fondles* Mr. Fuzzy Balls hehe *puts snout on balls and inhales deeply* oh god im so hard~ *licks balls* punish me daddy~ nyea~ *squirms more and wiggles butt* I love your musky goodness *bites lip* please punish me *licks lips* nyea~ *suckles on your tip* so good *licks pre of your cock* salty goodness~ *eyes role back and goes balls deep* mmmm~ *moans and suckles* o3o";
+hitOrMiss = "Hit or miss I guess they never miss, huh? You got a boyfriend, I bet he doesn\'t kiss ya He gon\' find another girl and he won\'t miss ya He gon\' skrrt and hit the dab like Wiz Khalifa";
+mesothelioma = "Attention: If you or a loved one has been diagnosed with Mesothelioma you may to be entitled to financial compensation. Mesothelioma is a rare cancer linked to asbestos exposure. Exposure to asbestos in they Navy, shipyards, mills, heating, construction or the automotive industries may put you at risk. Please don\'t wait, call LAW USA today for a free legal consultation and financial information packet. Mesothelioma patients call now! LAW USA";
+darthPlaguies = "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It\’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.";
+beeMovie = "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don\'t care what humans think is impossible.";
+allStar = "Somebody once told me the world is gonna roll me I ain\'t the sharpest tool in the shed She was looking kind of dumb with her finger and her thumb In the shape of an \"L\" on her forehead Well the years start coming and they don\'t stop coming Fed to the rules and I hit the ground running Didn\'t make sense not to live for fun Your brain gets smart but your head gets dumb So much to do, so much to see So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go You\'ll never shine if you don\'t glow Hey now, you\'re an all-star, get your game on, go play Hey now, you\'re a rock star, get the show on, get paid And all that glitters is gold Only shooting stars break the mold It\'s a cool place and they say it gets colder You\'re bundled up now, wait till you get older But the meteor men beg to differ Judging by the hole in the satellite picture The ice we skate is getting pretty thin The water\'s getting warm so you might as well swim My world\'s on fire, how about yours? That\'s the way I like it and I never get bored Hey now, you\'re an all-star, get your game on, go play Hey now, you\'re a rock star, get the show on, get paid All that glitters is gold Only shooting stars break the mold Hey now, you\'re an all-star, get your game on, go play Hey now, you\'re a rock star, get the show, on get paid And all that glitters is gold Only shooting stars Somebody once asked could I spare some change for gas? I need to get myself away from this place I said yep what a concept I could use a little fuel myself And we could all use a little change Well, the years start coming and they don\'t stop coming Fed to the rules and I hit the ground running Didn\'t make sense not to live for fun Your brain gets smart but your head gets dumb So much to do, so much to see So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go You\'ll never shine if you don\'t glow Hey now, you\'re an all-star, get your game on, go play Hey now, you\'re a rock star, get the show on, get paid And all that glitters is gold Only shooting stars break the mold And all that glitters is gold Only shooting stars break the mold";
+pussy = "Pussy pussy pussy marijuana, ijuana Pussy pussy pussy marijuana Pussy pussy pussy marijuana, ijuana Pussy pussy pussy marijuana Pussy pussy pussy marijuana, I wanna Smoke weed everyday, and I\'m gonna Eat that pussy, period, comma, I pause But hold the applause I want to mold and fold the laws I\'ll be bold, unroll the gauze I want to touch your pee-pee hole Blunt in the other hand, watch me roll Don\'t got a watch, but watch my flow Don\'t got a clock, but clock them hoes Don\'t ever sleep, can\'t watch me doze Now I\'m back now, rollin\' in the lack now Gonna put a smackdown, to this fucking wack town P-U-S-S-Y \'til I die I might be fat, but my bitches stay fly Maybe it\'s because I\'m so high in the sky Don\'t got a job so la la la La la la I might fuck your mother one day La la la La la la";
+
+var canvas = document.getElementById('canvas');
+canvas.style.position = 'absolute';
+canvas.style.top = 0;
+canvas.style.left = 0;
+var context = canvas.getContext('2d');
+var input = "";
+var ticks = 30;
+var shift = false;
+var words = 3;
+var currX = 0;
+var currY = 0;
+var score = 0;
+var timer = 10000;
+var lose = false;
+var menu = true;
+var current = 0;
+var copypasta = [navySeals.split(" "), rickMorty.split(" "), chickenNuggets.split(" "), rawr.split(" "), hitOrMiss.split(" "), mesothelioma.split(" "), darthPlaguies.split(" "), beeMovie.split(" "), allStar.split(" "), pussy.split(" ")];
+var names = ["Navy Seals", "Rick and Morty", "Chicken Nuggets", "rawr x3 *nuzzles*", "Hit or Miss", "Mesothelioma", "Darth Plagueis", "Bee Movie Intro", "All Star", "Pussy Pussy Pussy Marijuana"];
+
+function render() {
+	if(menu)
+	{
+		for(var i in copypasta) {
+			context.strokeStyle = "rgba(0, 0, 0, 1)";
+			context.lineWidth = "5";
+			context.beginPath();
+			context.rect(canvas.width/2 - canvas.width/8, canvas.height*0.08 + canvas.height*0.08*i, canvas.width/4, canvas.height*0.07);
+			context.stroke();
+			context.closePath();
+			context.fillStyle = "rgba(0, 0, 0, 1)";
+			context.textAlign = "center";
+			context.font = "bold " + (canvas.width/80).toString() + 'px Arial';
+			context.fillText(names[i], canvas.width/2, canvas.height*0.12 + canvas.height*0.08*i);
+		}
+	} else {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		context.fillStyle = 'rgba(255, 255, 255, 1)';
+		context.fillRect(0, 0, canvas.width, canvas.height);
+
+		currX = 0;
+		currY = 0;
+		for(var i=0; i<words; i++) {
+			context.fillStyle = "rgba(0, 0, 0, 1)";
+			context.font = (canvas.width/106.67).toString() + 'px Arial';
+			context.fillText(copypasta[current][i] + " ", currX + canvas.width/10, currY + canvas.height/6);
+			var metrics = context.measureText(copypasta[current][i] + " ");
+			currX += metrics.width;
+
+			if(currX >= canvas.width*0.7)
+			{
+				currX = 0;
+				currY += canvas.width/106.67 + 7;
+			}
+		}
+
+		context.fillText(input, currX + canvas.width/10, currY + canvas.height/6);
+
+		context.fillStyle = "rgba(0, 0, 0, 1)";
+		context.font = "bold " + (canvas.width/106.67).toString() + 'px Arial';
+		context.fillText("Score: " + score, canvas.width/15, canvas.height/13);
+
+		context.fillStyle = "rgba(0, 0, 0, 1)";
+		context.fillRect(canvas.width*0.65, canvas.height/11, canvas.width/10, canvas.height/40);
+		context.fillStyle = "rgba(" + (255 - 255*(timer/10000)) + ", " + 255*(timer/10000) + ", 0, 1)";
+		context.fillRect((canvas.width*0.65 + 2) + ((canvas.width/10 - 4)/2)*(1 - (timer/10000)), canvas.height/11 + 2, (canvas.width/10 - 4) - (canvas.width/10 - 4)*(1 - (timer/10000)), canvas.height/40 - 4);
+
+		context.strokeStyle = "rgba(0, 0, 0, 1)";
+		context.lineWidth = "5";
+		context.beginPath();
+		context.rect(canvas.width/2 - canvas.width/10, canvas.height*0.8, canvas.width/5, canvas.height*0.1);
+		context.stroke();
+		context.closePath();
+
+		if(lose) {
+			context.fillStyle = "rgba(0, 0, 0, 1)";
+			context.textAlign = "center";
+			context.font = "bold " + (canvas.width/60).toString() + 'px Arial';
+			context.fillText("Return to Menu", canvas.width/2, canvas.height*0.85);
+			context.font = "bold " + (canvas.width/30).toString() + 'px Arial';
+			context.fillText("YOU LOST", canvas.width/2, canvas.height*0.4);
+			context.font = (canvas.width/60).toString() + 'px Arial';
+			context.fillText("The word was: " + copypasta[current][words], canvas.width/2, canvas.height*0.4 + canvas.width/30);
+			context.font = (canvas.width/60).toString() + 'px Arial';
+			context.fillText("Score: " + score, canvas.width/2, canvas.height*0.4 + canvas.width/15);
+		} else {
+			context.fillStyle = "rgba(0, 0, 0, 1)";
+			context.textAlign = "center";
+			context.font = "bold " + (canvas.width/60).toString() + 'px Arial';
+			context.fillText("Quit", canvas.width/2, canvas.height*0.85);
+		}
+	}
+}
+
+setInterval(function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context = canvas.getContext('2d');
+	context.imageSmoothingEnabled = false;
+
+	render();
+
+	if (!lose && !menu) {
+		if(input.replace(/[^a-zA-Z ]/g, "").replace(" ", "").toLowerCase() == copypasta[current][words].replace(/[^a-zA-Z ]/g, "").replace(" ", "").toLowerCase()) {
+			input = "";
+			score += copypasta[current][words].replace(/[^a-zA-Z ]/g, "").replace(" ", "").length;
+			timer = 10000;
+			words += 1;
+		}
+
+		timer -= 1000/ticks;
+
+		if(timer <= 0) {
+			lose = true;
+		}
+	}
+}, 1000/ticks);
+
+document.addEventListener('mouseup', function(event) {
+	mouseX = event.clientX;
+	mouseY = event.clientY;
+
+	if(!menu && mouseX >= canvas.width/2 - canvas.width/10 && mouseX <= canvas.width/2 + canvas.width/10 && mouseY >= canvas.height*0.8 && mouseY <= canvas.height*0.9) {
+		menu = true;
+		lose = false;
+	} else if(menu) {
+		for(var i in copypasta) {
+			context.rect(canvas.width/2 - canvas.width/8, canvas.height*0.08 + canvas.height*0.08*i, canvas.width/4, canvas.height*0.07);
+
+			if(mouseX > canvas.width/2 - canvas.width/8 && mouseX < canvas.width/2 + canvas.width/8 && mouseY > canvas.height*0.08 + canvas.height*0.08*i && mouseY < canvas.height*0.08 + canvas.height*0.08*i + canvas.height*0.07) {
+				current = i;
+				menu = false;
+				lose = false;
+				words = 3;
+				score = 0;
+				input = "";
+			}
+		}
+	}
+});
+
+document.addEventListener('keydown', function(event) {
+  input = input + keycode(event.keyCode, shift);
+
+  switch (event.keyCode) {
+    case 8: // Backspace
+      if(input.length > 0) {
+        input = input.slice(0, -1);
+      }
+      break;
+    case 16: // Shift
+      shift = true;
+      break;
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 16: // Shift
+      shift = false;
+      break;
+  }
+});
+
+function keycode(keycode, shift) {
+  switch (keycode) {
+    case 32: // Space
+      return ' ';
+    case 48:
+      return ((shift) ? ')' : '0');
+      break;
+    case 49:
+      return ((shift) ? '!' : '1');
+      break;
+    case 50:
+      return ((shift) ? '@' : '2');
+      break;
+    case 51:
+      return ((shift) ? '#' : '3');
+      break;
+    case 52:
+      return ((shift) ? '$' : '4');
+      break;
+    case 53:
+      return ((shift) ? '%' : '5');
+      break;
+    case 54:
+      return ((shift) ? '^' : '6');
+      break;
+    case 55:
+      return ((shift) ? '&' : '7');
+      break;
+    case 56:
+      return ((shift) ? '*' : '8');
+      break;
+    case 57:
+      return ((shift) ? '(' : '9');
+      break;
+    case 65: // A
+      return ((shift) ? 'A' : 'a');
+      break;
+    case 66:
+      return ((shift) ? 'B' : 'b');
+      break;
+    case 67:
+      return ((shift) ? 'C' : 'c');
+      break;
+    case 68:
+      return ((shift) ? 'D' : 'd');
+      break;
+    case 69:
+      return ((shift) ? 'E' : 'e');
+      break;
+    case 70:
+      return ((shift) ? 'F' : 'f');
+      break;
+    case 71:
+      return ((shift) ? 'G' : 'g');
+      break;
+    case 72:
+      return ((shift) ? 'H' : 'h');
+      break;
+    case 73:
+      return ((shift) ? 'I' : 'i');
+      break;
+    case 74:
+      return ((shift) ? 'J' : 'j');
+      break;
+    case 75:
+      return ((shift) ? 'K' : 'k');
+      break;
+    case 76:
+      return ((shift) ? 'L' : 'l');
+      break;
+    case 77:
+      return ((shift) ? 'M' : 'm');
+      break;
+    case 78:
+      return ((shift) ? 'N' : 'n');
+      break;
+    case 79:
+      return ((shift) ? 'O' : 'o');
+      break;
+    case 80:
+      return ((shift) ? 'P' : 'p');
+      break;
+    case 81:
+      return ((shift) ? 'Q' : 'q');
+      break;
+    case 82:
+      return ((shift) ? 'R' : 'r');
+      break;
+    case 83:
+      return ((shift) ? 'S' : 's');
+      break;
+    case 84:
+      return ((shift) ? 'T' : 't');
+      break;
+    case 85:
+      return ((shift) ? 'U' : 'u');
+      break;
+    case 86:
+      return ((shift) ? 'V' : 'v');
+      break;
+    case 87:
+      return ((shift) ? 'W' : 'w');
+      break;
+    case 88:
+      return ((shift) ? 'X' : 'x');
+      break;
+    case 89:
+      return ((shift) ? 'Y' : 'y');
+      break;
+    case 90:
+      return ((shift) ? 'Z' : 'z');
+      break;
+    case 186:
+      return ((shift) ? ':' : ';');
+      break;
+    case 187:
+      return ((shift) ? '+' : '=');
+      break;
+    case 188:
+      return ((shift) ? '<' : ',');
+      break;
+    case 189:
+      return ((shift) ? '_' : '-');
+      break;
+    case 190:
+      return ((shift) ? '>' : '.');
+      break;
+    case 191:
+      return ((shift) ? '?' : '/');
+      break;
+    case 192:
+      return ((shift) ? '~' : '`');
+      break;
+    case 219:
+      return ((shift) ? '{' : '[');
+      break;
+    case 220:
+      return ((shift) ? '|' : '\\');
+      break;
+    case 221:
+      return ((shift) ? '}' : ']');
+      break;
+    case 222:
+      return ((shift) ? '"' : "'");
+      break;
+    case 96: // NUMPAD begins here
+      return '0';
+      break;
+    case 97:
+      return '1';
+      break;
+    case 98:
+      return '2';
+      break;
+    case 99:
+      return '3';
+      break;
+    case 100:
+      return '4';
+      break;
+    case 101:
+      return '5';
+      break;
+    case 102:
+      return '6';
+      break;
+    case 103:
+      return '7';
+      break;
+    case 104:
+      return '8';
+      break;
+    case 105:
+      return '9';
+      break;
+    case 106:
+      return '*';
+      break;
+    case 107:
+      return '+';
+      break;
+    case 109:
+      return '-';
+      break;
+    case 110:
+      return '.';
+      break;
+    case 111:
+      return '/';
+      break;
+    default:
+      return '';
+  }
+}
